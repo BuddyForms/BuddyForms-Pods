a6cc2e3727e1cb4d2b99bea35631dcc7dc169edc
svenl77 <mail@sven-lehnert.de>
2020-01-20 09:18:20 +0000

    * Added js file for the validation * Hack me into the form element to make the validation work.

--- includes/functions.php
+++ includes/functions.php
@@ -5,6 +5,7 @@
  */
 function buddyforms_pods_include_assets() {
 	wp_enqueue_style( 'buddyforms_pods_css', BUDDYFORMS_PODS_ASSETS_URL . "css/pods-form.css" );
+	wp_enqueue_script( 'buddyforms_pods_js', BUDDYFORMS_PODS_ASSETS_URL . 'js/pods-form.js', array( 'jquery', 'buddyforms-js' ), '1.4.0' );
 }
 
 add_action( 'buddyforms_front_js_css_after_enqueue', 'buddyforms_pods_include_assets');
